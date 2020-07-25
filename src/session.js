00b236129b0f4fbf469229a58f906b61d084e5c6
Dmitriy Tsvettsikh <dmitrycvet@gmail.com>
2020-07-25 15:46:04 +0500

    set up resolver from env DNSCRYPT_RESOLVER

--- src/session.js
+++ src/session.js
@@ -4,11 +4,25 @@ const Emitter = require('events');
 const dnsstamp = require('dnsstamp').DNSStamp;
 const debug = require('debug')('dnscrypt');
 
+const { DNSCRYPT_RESOLVER } = process.env;
+
 // Default is quad9 - no-dnssec, no-log, no-filter resolver.
 const DEFAULT_RESOLVER =
   'sdns://AQYAAAAAAAAADTkuOS45LjEwOjg0NDMgZ8hHuMh1jNEgJFVDvnVnRt803x2EwAuMRwNo34Idhj4ZMi5kbnNjcnlwdC1jZXJ0LnF1YWQ5Lm5ldA';
+let MAIN_RESOLVER = DEFAULT_RESOLVER;
 
 const DEFAULT_TIMEOUT = 2e3;
+
+// Set up resolver from env.
+if (typeof DNSCRYPT_RESOLVER === 'string') {
+  try {
+    dnsstamp.parse(DNSCRYPT_RESOLVER);
+    MAIN_RESOLVER = DNSCRYPT_RESOLVER;
+    debug('set up resolver from env');
+  } catch (err) {
+    debug('unable to parse DNSCRYPT_RESOLVER env', err);
+  }
+}
 
 /**
  * This class implements DNSCrypt session.
@@ -19,7 +33,7 @@ class Session extends Emitter {
    */
   constructor() {
     super();
-    this.setResolver(DEFAULT_RESOLVER);
+    this.setResolver(MAIN_RESOLVER);
 
     this.certificate = null; // Resolver certificate.
     this.certificateTimeout = DEFAULT_TIMEOUT;
@@ -44,6 +58,6 @@ class Session extends Emitter {
 
 module.exports = {
   Session,
-  DEFAULT_RESOLVER,
+  DEFAULT_RESOLVER: MAIN_RESOLVER,
   DEFAULT_TIMEOUT,
 };
